66e937374b5ef5b394d15a7d1d0ec0fd98728237
Heinrich Weinz <heinrich@AZ>
2014-12-22 20:08:42 -0200

    QtSql included

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QtSql/QSqlDatabase>
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    if (!QSqlDatabase::drivers().contains("QSQLITE"))
+        QMessageBox::critical(this, "Unable to load database", "This demo needs the SQLITE driver");
+
 }
 
 MainWindow::~MainWindow()
